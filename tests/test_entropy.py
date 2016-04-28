67c816f74767d69d25fc28e7d50e6b084f99f6ce
Kenneth Lyons <ixjlyons@gmail.com>
2016-04-28 14:19:43 -0700

    Added test for pattern matrix generation.

--- tests/test_entropy.py
+++ tests/test_entropy.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 from unittest import TestCase
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_equal, assert_almost_equal
 
 import entropy
 
@@ -20,3 +20,18 @@ class TestEntropy(TestCase):
     def test_fuzzyen_1d(self):
         e = entropy.fuzzyen(self.data_1d, 2, 0.1, 2)
         assert_almost_equal(e, 0.20953354)
+
+    def test_pattern_mat(self):
+        x = list(range(8))
+
+        targ3 = [[0, 1, 2, 3, 4, 5],
+                 [1, 2, 3, 4, 5, 6],
+                 [2, 3, 4, 5, 6, 7]]
+
+        targ4 = [[0, 1, 2, 3, 4],
+                 [1, 2, 3, 4, 5],
+                 [2, 3, 4, 5, 6],
+                 [3, 4, 5, 6, 7]]
+
+        assert_equal(entropy.pattern_mat(x, 3), targ3)
+        assert_equal(entropy.pattern_mat(x, 4), targ4)
